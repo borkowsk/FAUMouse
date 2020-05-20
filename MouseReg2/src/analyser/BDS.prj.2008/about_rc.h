5dd7bcb4d0c5e61e3ebd73e3b1294c2c3f53cd55
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-05-20 12:23:52 +0200

    Modyfikacje_analizatora_w_2008

---
+++ MouseReg2/src/analyser/BDS.prj.2008/about_rc.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by about.rc
+//
+
+#define IDS_APPNAME                     1
+#define IDS_DESCRIPTION                 2
+#define IDI_APPICON                     101
+#define IDC_CURSOR1                     102
+#define IDI_ICON1                       106
+#define IDD_DIALOG_ABOUT                107
+#define IDB_BITMAP1                     110
+#define IDB_BITMAP2                     112
+#define IDC_STATIC                      -1
+#define IDM_EXIT                        1000
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40027
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
