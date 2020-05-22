e6ec1c0a14072ce80c8c444d0bf4781f7cc9f135
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-05-22 20:39:18 +0200

    start.sh modified for more flexibility

--- MouseReg3/start.sh
+++ MouseReg3/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MYADDRES=localhost #For debuging only!
+MYPORT=8889
 BROWSER=firefox #Select proper LOCAL browser
 #MYADDRES=192.168.1.100 #Select the proper IP
 
@@ -14,9 +15,13 @@ source "screen.ini"
 echo -e $COLOR1 STARTING SERVICE $COLOR2
 (./fasada-core/hello > service.log 2>&1 &)\
  && (sleep 1 )\
- && (./fasada-core/wwwserver  $MYADDRES 8888 ./public/ &)\
+ && (./fasada-core/wwwserver  $MYADDRES $MYPORT ./public/ &)\
  && (sleep 1 )\
- && ($BROWSER http://$MYADDRES:8888/ >> service.log 2>&1 &)\
+ && ($BROWSER http://$MYADDRES:$MYPORT/ >> service.log 2>&1 &)\
+ && echo "$BROWSER http://$MYADDRES:$MYPORT/!!!!" > stop.sh \
+ && chmod +x stop.sh \
  && echo -e $COLOR1 For close send $COLOR2\
-            "http://$MYADDRES:8888/!!!!"\
-            $COLOR1 from $BROWSER $NORMCO
+            "http://$MYADDRES:$MYPORT/!!!!"\
+            $COLOR1 from $BROWSER $NORMCO "\n"
+            $COLOR1 Or use $COLOR2 stop.sh $COLOR1 in this directory $NORMCO
+
