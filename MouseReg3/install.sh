ced726b1b9b48d47cfa64bcac9585353a59a301f
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-05-22 14:43:26 +0200

    Starting_scripts_introduced

---
+++ MouseReg3/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#TERMINAL CONFIG
+if [ ! -e "screen.ini" ]
+then
+  cp  "fasada-core/lib/fasada/_config_template/screen.ini" ./
+fi
+source "screen.ini"
+
+#REAL JOB
+pushd fasada-core
+echo -e $COLOR1 GIT JOB $NORMCO \
+&& git checkout master \
+&& git pull \
+&& echo -e $COLOR1 CMAKE JOB $COLOR2 \
+&& cmake . \
+&& echo -e $COLOR1 MAKE JOB $NORMCO \
+&& make \
+&& echo -e $COLOR1 DONE $NORMCO
+popd
+
---
+++ MouseReg3/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+MYADDRES=localhost #For debuging only!
+BROWSER=firefox
+#MYADDRES=<public-IP>
+
+#TERMINAL CONFIG
+if [ ! -e "screen.ini" ]
+then
+  cp  "fasada-core/lib/fasada/_config_template/screen.ini" ./
+fi
+source "screen.ini"
+
+#REAL JOB
+echo -e $COLOR1 STARTING SERVICE $COLOR2
+(./fasada-core/hello > service.log 2>&1 &)\
+ && (sleep 1 )\
+ && (./fasada-core/wwwserver  $MYADDRES 8888 ./MouseRegJs/ &)\
+ && (sleep 1 )\
+ && ($BROWSER http://$MYADDRES:8888/ >> service.log 2>&1 &)\
+ && echo -e $COLOR1 For close send $COLOR2\
+            "http://$MYADDRES:8888/!!!!"\
+            $COLOR1 from $BROWSER $NORMCO
