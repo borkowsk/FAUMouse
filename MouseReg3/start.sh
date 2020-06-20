a152bd201fb7ba76d35b0a3ec31373b90a2fbd4d
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-06-20 16:49:36 +0200

    Start_modified_for_using_src_directory

--- MouseReg3/start.sh
+++ MouseReg3/start.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 BROWSER=firefox #Select proper LOCAL browser
-MYADDRES=localhost #For debuging only!
+MYADDRES=localhost #For debbuging only!
 MYPORT=8889
-#MYADDRES=192.168.1.100 #Select the proper IP
-#MYPORT=8989
+#MYADDRES=192.168.1.100 #Select the IP proper for testing
+#MYPORT=8989    #Select the proper PORT for testing, when 8989 is not available
+
+if [ $# -eq 2 ]
+then
+ #echo "2 arguments supplied"
+ MYADDRES="$1"
+ MYPORT="$2"    
+fi
 
 #TERMINAL CONFIG
 if [ ! -e "screen.ini" ]
@@ -14,7 +21,7 @@ source "screen.ini"
 
 #REAL JOB
 echo -e $COLOR1 STARTING SERVICE $COLOR2
-(./fasada-core/hello > service.log 2>&1 &)\
+(./src/regmouse3 > service.log 2>&1 &)\
  && (sleep 1 )\
  && (./fasada-core/wwwserver  $MYADDRES $MYPORT ./public/ &)\
  && (sleep 1 )\
