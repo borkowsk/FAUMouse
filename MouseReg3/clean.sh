202332da51d08b20c774ac6af0f2aea79c9eda7d
Wojciech T. Borkowski <wborkowsk@gmail.com>
2020-07-21 18:51:30 +0200

    Directory cleaning script

---
+++ MouseReg3/clean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source "screen.ini"
+echo -e $COLOR1"All non-needed files in '" `pwd` "' will be cleaned"$COLOR2
+echo -e "Press Ctrl-C in 4 seconds!"$NORMCO
+sleep 4
+rm -f service.log
+rm -f .qtcreator.err
+rm -f *.*~
+rm -f IPs.txt
+rm -f stop.sh
+rm -f src/*.cmake
+rm -f src/CMakeCache.txt
+rm -f src/CMakeLists.txt.user
+rm -f src/Makefile
+rm -R -f src/CMakeFiles
+rm -R -f build-*
+pushd fasada-core/
+./clean.sh
+popd
